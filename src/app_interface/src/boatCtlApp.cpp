39b81c8a31e025297bee0523ae9c0b14b5749fd4
shuai <1223391749@qq.com>
2024-12-02 18:57:23 +0800

    update

--- src/app_interface/src/boatCtlApp.cpp
+++ src/app_interface/src/boatCtlApp.cpp
@@ -4,7 +4,6 @@
 
 int main(int argc, char **argv)
 {
-    QApplication::addLibraryPath(R"(/home/shuai/Qt/5.15.2/gcc_64/plugins)");
     QApplication a(argc, argv);
     MainWindow w(nullptr,argc,argv);
     w.show();
